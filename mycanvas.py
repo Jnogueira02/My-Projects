c95e69ef89eded0b4a4975d2fc6d1dfb96e4ed8e
James Nogueira <112129760+Jnogueira02@users.noreply.github.com>
2023-10-07 12:36:09 -0500

    Update mycanvas.py

--- mycanvas.py
+++ mycanvas.py
@@ -1,11 +1,13 @@
+# Import the Canvas class from reportlab (for creating and editing PDFs)
 from reportlab.pdfgen import canvas
 
-
+# Add a current_font attribute to the canvas class
 class MyCanvas(canvas.Canvas):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.current_font = None
 
+# Set the current_font for the canvas object
     def setFont(self, fontname, fontsize):
         self.current_font = fontname
         super().setFont(fontname, fontsize)
