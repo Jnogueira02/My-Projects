11119ca2e9fc9bff072cafb86d08cd0a05d4340e
James Nogueira <112129760+Jnogueira02@users.noreply.github.com>
2023-10-07 12:37:16 -0500

    Update fadedimage2.py

--- fadedimage2.py
+++ fadedimage2.py
@@ -1,3 +1,10 @@
+# Import modules:
+
+# MyCanvas for editing PDFs
+# PIL for handling images
+# NumPy for scaling arrays (used for adjusting image dimensions)
+# Requests and BytesIO for accessing photos from the web
+# Time for timing
 from mycanvas import MyCanvas
 from PIL import Image
 import numpy as np
@@ -7,6 +14,7 @@ from io import BytesIO
 import time
 
 
+# Scale image dimensions to fit into PDF box
 def scale_np_array(array_data, limit):
     scaling_factor = limit / array_data[0]
     scaled_np_array = array_data * scaling_factor
@@ -53,11 +61,12 @@ class FadedImage2:
             self.y = self.y - (my_scaled_dimensions[1] / 2)
 
             self.canvas.setFillAlpha(0.5)
-            self.canvas.drawImage(f"MyPica44edtfbdv5yygr{self.count}.jpg", self.x, self.y + 26.8, my_scaled_dimensions[0], my_scaled_dimensions[1])
+            self.canvas.drawImage(f"MyPica44edtfbdv5yygr{self.count}.jpg", self.x, self.y + 26.8,
+                                  my_scaled_dimensions[0], my_scaled_dimensions[1])
             self.canvas.setFillAlpha(1)
 
             print(f"print_pic_2 time: {time.time() - t1}")
-            return f"MyPica44edtfbdv5yygr{self.count}.jpg" ##Need to change this??
+            return f"MyPica44edtfbdv5yygr{self.count}.jpg"  ##Need to change this??
 
     def print_pic_downloaded(self, opacity):
         t1 = time.time()
@@ -74,17 +83,10 @@ class FadedImage2:
         self.x = self.x - (my_scaled_dimensions[0] / 2)
         self.y = self.y - (my_scaled_dimensions[1] / 2)
 
-        # self.canvas.setFillAlpha(0.5)
-
         self.canvas.setFillAlpha(int(opacity) / 100)
 
-        # self.canvas.drawImage(self.image_url, self.x, self.y + 26.8, my_scaled_dimensions[0], my_scaled_dimensions[1])
-        # self.canvas.drawImage(self.image_url, self.x-20, self.y + 26.8, my_scaled_dimensions[0], my_scaled_dimensions[1])
-        # self.canvas.drawImage(self.image_url, self.x-2, self.y + 26.8, my_scaled_dimensions[0], my_scaled_dimensions[1])
-        # self.canvas.drawImage(self.image_url, self.x-3, self.y + 26.8, my_scaled_dimensions[0], my_scaled_dimensions[1])
-        # self.canvas.drawImage(self.image_url, self.x-3.1, self.y + 26.8, my_scaled_dimensions[0], my_scaled_dimensions[1])
-        # self.canvas.drawImage(self.image_url, self.x-3.2, self.y + 26.8, my_scaled_dimensions[0], my_scaled_dimensions[1])
-        self.canvas.drawImage(self.image_url, self.x-3.15, self.y + 26.8, my_scaled_dimensions[0], my_scaled_dimensions[1])
+        self.canvas.drawImage(self.image_url, self.x - 3.15, self.y + 26.8, my_scaled_dimensions[0],
+                              my_scaled_dimensions[1])
         self.canvas.setFillAlpha(1)
 
         print(f"print_pic_2 time: {time.time() - t1}")
